f7cb339f4156d330dc0214fc8c75e843a7953652
a.Cincarevic <acoaco908@gmail.com>
2018-03-18 05:01:33 +0100

    Update ES6goodies.js

--- ES6goodies.js
+++ ES6goodies.js
@@ -59,3 +59,58 @@ var Fredde = {
   }
 }
 Fredde.printFriends();
+
+// Foreach
+
+var sample = [1, 2, 3];
+// es5
+sample.forEach(function (elem, index){
+   console.log(elem + ' comes at ' + index);
+})
+// es6
+sample.forEach((elem, index) => `${elem} at index ${index}`)
+/*
+output
+1 at index 0
+2 at index 1
+3 at index 2
+*/
+
+//--> Filter
+
+var sample1 = [1, 2, 3] // yeah! samma array
+// es5
+var result = sample.filter(function(elem){
+    return elem !== 2;
+})
+console.log(result)
+// es6
+var result = sample.filter(elem => elem !== 2)
+/* output */
+//[1, 3]
+
+
+//--> Map
+
+var sample2 = [1, 2, 3];
+// es5
+var mapped = sample2.map(function(elem) {
+    return elem * 10;
+})
+// es6
+let mapped = sample2.map(elem => elem * 10)
+console.log(mapped);
+/* output */
+//[10, 20, 30]
+
+
+//-->  Reduce
+
+var sampl3 = [1, 2, 3] // here we meet again
+// es5
+var sum = sample3.reduce(function(sum, elem){
+    return sum + elem;
+})
+// es6
+var sum = sample3.reduce((sum, elem) => sum + elem)
+console.log(sum)
